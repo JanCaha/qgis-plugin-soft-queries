fbf4e5cd7520a0585612a79ef21f1b3000fb89b7
Jan Caha <jan.caha@outlook.com>
2022-05-30 21:28:41 +0200

    if help does not exist return empty string

--- soft_queries/expressions/qgsexpressions_utils.py
+++ soft_queries/expressions/qgsexpressions_utils.py
@@ -6,8 +6,12 @@ def load_help(function_name: str) -> str:
 
     path = Path(__file__).parent / "help_files" / "{}.html".format(function_name)
 
-    with open(path) as file:
-        help = file.read()
+    help = ""
+
+    if path.exists():
+
+        with open(path) as file:
+            help = file.read()
 
     return help
 
