5eaadf2d8389217d875e3f539ec9c9878b213e79
Jan Caha <jan.caha@outlook.com>
2023-10-04 21:16:59 +0200

    add deps folder to path

--- soft_queries/__init__.py
+++ soft_queries/__init__.py
@@ -2,10 +2,22 @@ __author__ = "Jan Caha"
 __date__ = "2022-03-01"
 __copyright__ = "(C) 2022 by Jan Caha"
 
+import sys
+from pathlib import Path
+
 from .plugin_soft_queries import SoftQueriesPlugin
+
+try:
+    import FuzzyMath
+except ModuleNotFoundError:
+    # if required modules are not available on system, let's use the versions that we package
+    this_dir = Path(__file__).parent
+    deps_dir = this_dir / "deps"
+    if deps_dir.exists():
+        for f in deps_dir.iterdir():
+            sys.path.append(f.as_posix())
 
 
 # noinspection PyPep8Naming
 def classFactory(iface):
-
     return SoftQueriesPlugin(iface)
